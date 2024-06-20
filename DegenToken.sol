b38e54c46066831cdc1e54200868879f297fff4f
Anurag Kaushik <anurag11b12345@gmail.com>
2024-06-20 21:03:28 +0530

    Update DegenToken.sol

--- DegenToken.sol
+++ DegenToken.sol
@@ -7,7 +7,7 @@
 */
 
 // SPDX-License-Identifier: MIT
-pragma solidity  ^0.8.0;
+pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
@@ -15,96 +15,101 @@ import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
 
 contract DegenToken is ERC20, Ownable, ERC20Burnable {
 
-    struct thingsInStore {
+    struct ThingInStore {
         uint ID;
         string nameofItem;
         uint256 price;
     }
 
-    thingsInStore[] public itemsinStore;
+    ThingInStore[] public itemsInStore;
 
-    constructor() ERC20("Degen","DGN") Ownable(msg.sender) {
-        itemsinStore.push(thingsInStore(1, "Sword", 100));
-        itemsinStore.push(thingsInStore(2, "car", 200));
-        itemsinStore.push(thingsInStore(3, "Bike", 150));
-        itemsinStore.push(thingsInStore(4, "Gun", 300));
-        itemsinStore.push(thingsInStore(5, "Shield", 400));
+    constructor() ERC20("Degen", "DGN") Ownable(msg.sender) {
+        itemsInStore.push(ThingInStore(1, "Sword", 100));
+        itemsInStore.push(ThingInStore(2, "Car", 200));
+        itemsInStore.push(ThingInStore(3, "Bike", 150));
+        itemsInStore.push(ThingInStore(4, "Gun", 300));
+        itemsInStore.push(ThingInStore(5, "Shield", 400));
     }
 
-    function mint(address mintAddress, uint256 _Amount) public onlyOwner {
-        _mint(mintAddress, _Amount);
+    function mint(address mintAddress, uint256 _amount) public onlyOwner {
+        _mint(mintAddress, _amount);
     }
 
-    function decimals() override public pure returns(uint8) {
-        return 0;
+    function decimals() public pure override returns (uint8) {
+        return 18; // Assuming standard ERC20 token with 18 decimals
     }
 
-    function transferTokens(address _reciever, uint256 _Amount) external {
-        require(_Amount > 0, "Transfer amount must be greater than zero!!");
-        require(balanceOf(msg.sender) >= _Amount, "Balance is low!");
-        _transfer(msg.sender, _reciever, _Amount);
+    function transferTokens(address _receiver, uint256 _amount) external {
+        require(_amount > 0, "Transfer amount must be greater than zero!!");
+        require(balanceOf(msg.sender) >= _amount, "Balance is low!");
+        _transfer(msg.sender, _receiver, _amount);
     }
 
-    function getBalance() external view returns (uint256){
-        return this.balanceOf(msg.sender);
+    function getBalance() external view returns (uint256) {
+        return balanceOf(msg.sender);
     }
 
-    function burnTokens(uint256 _Amount) external {
-        require(balanceOf(
-            msg.sender) >= _Amount,
-            "You do not have enough Degen tokens to burn"
-            );
-        _burn(msg.sender, _Amount);
+    function burnTokens(uint256 _amount) external {
+        require(balanceOf(msg.sender) >= _amount, "Insufficient balance to burn");
+        _burn(msg.sender, _amount);
     }
 
-    function convertInttoString(uint256 _index) internal pure returns (string memory){
-        if ( _index == 0 ) {
+    function convertInttoString(uint256 _index) internal pure returns (string memory) {
+        if (_index == 0) {
             return "0";
         }
 
-        uint256 vari = _index;
-        uint256 leng;
-        while ( vari != 0 ) {
-            leng++;
-            vari /= 10;
+        uint256 temp = _index;
+        uint256 length;
+        while (temp != 0) {
+            length++;
+            temp /= 10;
         }
 
-        bytes memory bym = new bytes(leng);
-        uint256 x = leng;
-        while ( _index != 0 ){
-            x--;
-            uint8 tempVar = (48 + uint8(_index - _index / 10 * 10));
-            bytes1 byt1 = bytes1(tempVar);
-            bym[x] = byt1;
+        bytes memory str = new bytes(length);
+        uint256 j = length;
+        while (_index != 0) {
+            j--;
+            uint8 tempByte = (48 + uint8(_index - _index / 10 * 10));
+            bytes1 char = bytes1(tempByte);
+            str[j] = char;
             _index /= 10;
         }
 
-        return string(bym);
+        return string(str);
     }
 
     function shopToRedeem() external view returns (string memory) {
-        string memory shopitems = "Available itmes in store:\n";
-        for (uint256 index = 0; index < itemsinStore.length; index++) {
-            shopitems = string(abi.encodePacked(
-            shopitems,
-            convertInttoString(index + 1),
-            ". ",
-            itemsinStore[index].nameofItem,
-            " - ",
-            convertInttoString(itemsinStore[index].price / (10 ** decimals())),
-            " DGN",
-            "\n"
+        string memory shopItems = "Available items in store:\n";
+        for (uint256 index = 0; index < itemsInStore.length; index++) {
+            shopItems = string(abi.encodePacked(
+                shopItems,
+                convertInttoString(index + 1),
+                ". ",
+                itemsInStore[index].nameofItem,
+                " - ",
+                convertInttoString(itemsInStore[index].price / (10 ** uint256(decimals()))),
+                " DGN",
+                "\n"
             ));
         }
-        return shopitems;
+        return shopItems;
     }
 
-    function redeemItems(uint256 itemIndex) public {
-        require(itemIndex < itemsinStore.length, 
-                "Invalid Index for an item");
-        thingsInStore memory item = itemsinStore[itemIndex];
-        require(balanceOf(msg.sender) >= item.price,
-                "Tokens are less in account!");
+    mapping(address => ThingInStore[]) public userItems;
+
+    function redeemItem(uint256 itemIndex) external {
+        require(itemIndex < itemsInStore.length, "Invalid index for item");
+        
+        ThingInStore memory item = itemsInStore[itemIndex];
+        require(balanceOf(msg.sender) >= item.price, "Insufficient balance to redeem item");
+
         _burn(msg.sender, item.price);
+
+        userItems[msg.sender].push(item);
+    }
+
+    function getUserItems(address user) external view returns (ThingInStore[] memory) {
+        return userItems[user];
     }
 }
